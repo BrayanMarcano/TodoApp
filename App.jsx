29dba7d252c6cecd4ae0f962d016b992e6d199f3
BrayanMarcano <brayan.marcano@xmartlabs.com>
2021-09-07 16:42:39 -0300

    Segunda Pantalla

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { InputUser } from "./InputUser";
+import { View } from "react-native";
+import { Headers } from "./Header";
+
+const App = () => (
+  <View>
+    <Headers />
+    <InputUser />
+  </View>
+);
+export default App;
