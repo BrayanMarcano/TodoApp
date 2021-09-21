6e768919fcee296574c9e41a465aa23f848ce925
BrayanMarcano <brayan.marcano@xmartlabs.com>
2021-09-21 15:05:47 -0300

    Structure files

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+export const Header = () => (
+  <View style={styles.container}>
+    <Text style={styles.left}>Cancel</Text>
+    <Text style={styles.main}>New task</Text>
+    <Text style={styles.right}>Save</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    height: 70,
+    paddingTop: 30,
+    backgroundColor: "#1F86FF",
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+  left: {
+    color: "#ffffff",
+    fontSize: 20,
+  },
+  main: {
+    textAlign: "center",
+    color: "#ffffff",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  right: {
+    textAlign: "right",
+    color: "#ffffff",
+    fontSize: 20,
+  },
+});
---
+++ src/screen/NewTask.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { InputUser } from "./src/components/InputUser";
+import { View } from "react-native";
+import { Header } from "./src/components/Header";
+
+const App = () => (
+  <View>
+    <Header />
+    <InputUser />
+  </View>
+);
+export default App;
